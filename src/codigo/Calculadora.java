4632409ed454d48b597e600b47ff93d6c481455b
Thiago Fernandes <tfpmotta@gmail.com>
2023-08-04 17:14:25 -0300

    Update Calculadora.java

--- src/codigo/Calculadora.java
+++ src/codigo/Calculadora.java
@@ -1,11 +1,12 @@
 package codigo;
 public class Calculadora {
     
-    public int somar(int num1, int num2) {
-               
+    public int somar(int num1, int num2) {       
         return (num1+num2);
     }
-    
+    public int somar(int num1, int num2) {       
+        return (num1+num2);
+    }
     public int subtrair (int num1, int num2) {
         return (num1 - num2);
     }
